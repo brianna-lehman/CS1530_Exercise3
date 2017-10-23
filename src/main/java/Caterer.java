6dac29d9d1374e8e930621f54deb0f15782ef546
brianna-lehman <brilehman29@gmail.com>
2017-10-23 13:52:38 -0400

    caterer

---
+++ src/main/java/Caterer.java
@@ -0,0 +1,27 @@
+public class Caterer {	
+
+	public int lazyCaterer(int n){
+		return (int) ((Math.pow(n,2)+n+2)/2);
+	}
+
+		public int tri(int value) {
+		if (value == 1) {
+			return 1;
+		}
+		return value + tri(value-1);
+	}
+	
+	public static void main(String[] args){
+		if(args.length > 2){
+			System.out.println("Too many args");
+			System.exit(1);
+		}
+
+		Caterer caterer = new Caterer();
+		int triangles = caterer.tri(Integer.parseInt(args[1]));
+		int pieces = caterer.lazyCaterer(Integer.parseInt(args[0]));
+		System.out.println("Lazy("+args[0]+") = "+pieces);
+		System.out.println("Tri("+args[0]+") = "+triangles);
+
+	}
+}
