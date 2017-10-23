a123e2a8628267beef57a2d90691b23c4936b8af
brianna-lehman <brilehman29@gmail.com>
2017-10-23 15:36:55 -0400

    final commit

--- src/main/java/Caterer.java
+++ src/main/java/Caterer.java
@@ -13,14 +13,19 @@ public class Caterer {
 	}
 
 	public static void main(String[] args){
-		if(args.length > 2){
+		if(args.length > 2) {
 			System.out.println("Too many args");
+			System.exit(1);
+		}
+		if (args.length < 1) {
+			System.out.println("Not enough args");
+			System.out.println("grade run -Dargs=[integer for lazy()] [integer for tri()]");
 			System.exit(1);
 		}
 
 		Caterer caterer = new Caterer();
-		int triangles = caterer.tri(Integer.parseInt(args[1]));
 		int pieces = caterer.lazyCaterer(Integer.parseInt(args[0]));
+		int triangles = caterer.tri(Integer.parseInt(args[1]));
 		System.out.println("Lazy("+args[0]+") = "+pieces);
 		System.out.println("Tri("+args[1]+") = "+triangles);
 
